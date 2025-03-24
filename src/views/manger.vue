b0c5c37250c07bae64f349bc2a699e584c58e8e0
ALIUERNA <2490097529@qq.com>
2025-03-24 13:20:02 +0800

    Initial commit

--- src/views/manger.vue
+++ src/views/manger.vue
@@ -2,6 +2,7 @@
 <!--  头部区域-->
   <div class="title">
     <div style="font-size: 20px;color: black"><span>丁正难藏泪</span></div>
+    <img src="../../assets/img/logo.png" alt="" style="width: 40px;height: 40px;margin-right: 10px">
     <div style="flex: 1"></div>
   </div>
 
